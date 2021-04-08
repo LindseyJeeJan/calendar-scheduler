6fb5d24d5017a71a09de42178169f21a85b8f0de
Lindsey JJ <ljjlauria@mac.com>
2021-04-08 06:57:07 -0700

    Added monment logic to display todays date in correct format. Also pulled current hour and applied conditional statements to determine if schedule hours are past, present or future and applied appropriate class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,40 @@
-console.log("working");
+var currentDay = $('#currentDay');
+var saveButtons = $('button.save-btn');
+var timeBlocks = $('.time-block');
+var hourBlocks = timeBlocks.find('.hour');
+
+function renderCurrentDay() {
+    var today = moment().format('dddd, MMMM Do');
+    currentDay.text(today);
+}
+
+function assignColorClasses() {
+    var currentTime = moment().format('HH'); 
+
+    $.each(hourBlocks, function() {
+        var blockTime = $(this).attr("data-time"); 
+        if (blockTime <= currentTime){
+            console.log("past");
+            $(this).siblings('.row').addClass('past');
+        } else if(blockTime >= currentTime){
+            console.log("future");
+            $(this).siblings('.row').addClass('future');
+        } else {
+            console.log("present");
+            $(this).siblings('.row').addClass('present');
+        }
+    });
+
+}
+
+saveButtons.on('click', function(event) {
+    var buttonClicked = $(event.target);
+    console.log('Button Clicked', buttonClicked);
+});
+
+function init(){
+    renderCurrentDay();
+    assignColorClasses();
+}
+
+init();
